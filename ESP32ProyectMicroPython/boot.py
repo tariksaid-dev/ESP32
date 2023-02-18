53c1a57eda21b9b85f3e90e7f75265841b02d438
tariksaid-dev <tariksaid.dev@gmail.com>
2023-02-18 13:30:55 +0100

    Credenciales de archivo de texto terminadas. Socket no bloqueado, servidor ok y html ok

--- ESP32ProyectMicroPython/boot.py
+++ ESP32ProyectMicroPython/boot.py
@@ -8,6 +8,43 @@ import network
 
 import esp
 esp.osdebug(None)
+try:
+  import usocket as socket
+except:
+  import socket
+
+from machine import Pin
+import network
+
+import esp
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+credentials_txt = []
+
+with open('credentials.txt', 'r') as credentials:
+  linea = credentials.readline()
+  while linea:
+     credentials_txt.append(linea.strip())
+     linea = credentials.readline()
+
+ssid = credentials_txt[0]
+password = credentials_txt[1]
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+print(f'Conectado correctamente a la red {ssid}')
+print(station.ifconfig())
+
+#led = Pin(2, Pin.OUT)
 
 import gc
 gc.collect()
--- ESP32ProyectMicroPython/main.py
+++ ESP32ProyectMicroPython/main.py
@@ -1,6 +1,7 @@
 import ujson as json
 import urequests as requests
 import time
+import select
 
 def make_request(url):
     print("Cargando los datos...")
@@ -24,9 +25,27 @@ def get_hora(url):
     else:
         print("Error en la API, no se pudo obtener la hora")
 
+def web_page():
+  html = f"""<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>{hora_ultima_actualizacion}</h1>
+</body>
+</html>"""
+  return html
+
+
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 80))
 s.listen(5)
+print("Socket creado correctamente")
 
 # Api de semi/pago, cambiar en producto final o pagar 9$ mes para uso ilimitado
 URL_API_HORA = "https://timezone.abstractapi.com/v1/current_time/?api_key=1efba11478714b6599e5d95b2b0762f1&location=Madrid,%Spain"
@@ -35,15 +54,26 @@ INTERVALO = 60000
 
 hora_ultima_actualizacion = ""
 hours = []
+#["00-01, 00-02"]
 prices = []
+#[162.45, 184.98]
 ultima_actualizacion = time.ticks_ms()
 
-
-
 while True:
+    ready_to_read, _, _ = select.select([s], [], [], 1)
+    if s in ready_to_read:
+        conn, addr = s.accept()
+        print(f'Se ha conectado al socket {addr}')
+        request = conn.recv(1024)
+        response = web_page()
+        conn.send('HTTP/1.1 200 OK\n')
+        conn.send('Content-Type: text/html\n')
+        conn.send('Connection: close\n\n')
+        conn.sendall(response)
+        conn.close()
+
     if time.ticks_diff(time.ticks_ms(), ultima_actualizacion) >= INTERVALO:
         make_request(URL_API_LUZ)
         ultima_actualizacion = time.ticks_ms()
         hora_ultima_actualizacion = get_hora(URL_API_HORA)
         print(hora_ultima_actualizacion)
-
